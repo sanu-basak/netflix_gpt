6b154f0621b6649fc0fcdcb1e256162613205c30
Sanu <sanu.bhumca2014@gmail.com>
2024-03-15 17:57:25 +0530

    Error resolve

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,8 +2,8 @@ export const LOGO="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-89723
 export const AVATAR_LOGO="https://occ-0-4412-3647.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229"
 export const BG_IMAGE="https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/94eb5ad7-10d8-4cca-bf45-ac52e0a052c0/IN-en-20240226-popsignuptwoweeks-perspective_alpha_website_large.jpg"
 export const USER_AVATAR="https://avatars.githubusercontent.com/u/29189226?v=4"
-export const ACCESS_TOKEN="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNzY1ODBkNDQyNGM3MmQyOThjOTVjY2VlODcyOWUxMiIsInN1YiI6IjY1ZjE4ZWFkZDY0YWMyMDE2NDVlMjc0ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wRkuWTjxSh8xGcl57MBaxWNKAboRAf6pIGvlxahZRiI"
-export const API_KEY="a76580d4424c72d298c95ccee8729e12"
+// export const ACCESS_TOKEN="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNzY1ODBkNDQyNGM3MmQyOThjOTVjY2VlODcyOWUxMiIsInN1YiI6IjY1ZjE4ZWFkZDY0YWMyMDE2NDVlMjc0ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wRkuWTjxSh8xGcl57MBaxWNKAboRAf6pIGvlxahZRiI"
+// export const API_KEY="a76580d4424c72d298c95ccee8729e12"
 export const API_OPTIONS = {
     method: 'GET',
     headers: {
@@ -16,4 +16,3 @@ export const SUPPORTED_LANGUAGE = [
   { identifier: "hi", name: "Hindi"},
   { identifier: "sp", name: "Spanish"}
 ]
-// export const OPEN_AI_KEY="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"
