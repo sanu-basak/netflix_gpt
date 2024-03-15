08f8dc625959bdcaa6fe47ca9c263970de009538
Sanu <sanu.bhumca2014@gmail.com>
2024-03-15 19:43:22 +0530

    fix

---
+++ src/utils/constant.js
@@ -0,0 +1,17 @@
+export const LOGO="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+export const AVATAR_LOGO="https://occ-0-4412-3647.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229"
+export const BG_IMAGE="https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/94eb5ad7-10d8-4cca-bf45-ac52e0a052c0/IN-en-20240226-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+export const USER_AVATAR="https://avatars.githubusercontent.com/u/29189226?v=4"
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer '+process.env.REACT_APP_TMDB_ACCESS_TOKEN
+    }
+}
+export const SUPPORTED_LANGUAGE = [
+  { identifier: "en", name: "English"},
+  { identifier: "hi", name: "Hindi"},
+  { identifier: "sp", name: "Spanish"}
+]
+export const OPEN_AI_KEY=process.env.REACT_APP_OPEN_AI_KEY
